98b6ed2b1143218702debc512afea3440c9ac1b3
Makus14 <mesha.maksim14@gmail.com>
2024-11-19 10:02:44 +0300

    create style to tabContent

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { CORE_CONCEPTS } from './data.js'
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
-import TabButton from './components/TabButton/TubButton.jsx';
+import TabButton from './components/TabButton/TabButton.jsx';
 import { EXAMPLES } from './data.js';
 
 function App() {
@@ -52,10 +52,26 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton 
+              isSelected={selectedTopic === 'components'} 
+              onSelect={() => handleSelect('components')}>
+                Components
+            </TabButton>
+            <TabButton 
+              isSelected={selectedTopic === 'jsx'} 
+              onSelect={() => handleSelect('jsx')}>
+                JSX
+            </TabButton>
+            <TabButton 
+              isSelected={selectedTopic === 'props'} 
+              onSelect={() => handleSelect('props')}>
+                Props
+            </TabButton>
+            <TabButton 
+              isSelected={selectedTopic === 'state'} 
+              onSelect={() => handleSelect('state')}>
+                State
+            </TabButton>
           </menu>
           {tabContent}
         </section>
